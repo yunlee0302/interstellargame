8242c17ec05cc3d74d560075c211d7c7b7f805e3
yunlee0302 <82069263+yunlee0302@users.noreply.github.com>
2021-07-06 00:21:32 +0800

    orders sql更新

--- sql/orders.sql
+++ sql/orders.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost
--- 產生時間： 2021 年 07 月 02 日 11:23
+-- 產生時間： 2021 年 07 月 05 日 18:19
 -- 伺服器版本： 10.4.18-MariaDB
 -- PHP 版本： 8.0.3
 
@@ -29,14 +29,14 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `orders` (
   `orderId` int(11) NOT NULL COMMENT '流水號',
-  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '使用者帳號',
+  `userName` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '使用者帳號',
   `paymentTypeId` int(11) DEFAULT NULL COMMENT '付款方式',
   `deliveryTypeId` int(11) NOT NULL COMMENT '配送方式',
   `voucherId` int(11) NOT NULL COMMENT '優惠券',
-  `ordername` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '訂購姓名',
-  `ordermobile` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '訂購電話',
-  `orderemail` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '訂購email',
-  `orderaddress` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '訂購地址',
+  `orderName` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '訂購姓名',
+  `orderMobile` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '訂購電話',
+  `orderMail` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '訂購email',
+  `orderAddress` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '訂購地址',
   `created_at` datetime NOT NULL DEFAULT current_timestamp() COMMENT '新增時間',
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新時間'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='訂單資料表';
@@ -51,7 +51,7 @@ CREATE TABLE `orders` (
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`orderId`),
   ADD KEY `paymentTypeId` (`paymentTypeId`),
-  ADD KEY `orders_ibfk_1` (`username`),
+  ADD KEY `orders_ibfk_1` (`userName`),
   ADD KEY `deliveryTypeId` (`deliveryTypeId`),
   ADD KEY `voucherId` (`voucherId`);
 
