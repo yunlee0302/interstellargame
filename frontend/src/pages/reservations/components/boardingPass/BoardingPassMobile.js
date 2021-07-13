ea97a6a3035a9ddb239174d688f2606b4390a7c0
JCEleanor <crcky4826@gmail.com>
2021-07-13 23:09:44 +0800

    20210713: reservations: fix boarding pass mobile css

--- frontend/src/pages/reservations/components/boardingPass/BoardingPassMobile.js
+++ frontend/src/pages/reservations/components/boardingPass/BoardingPassMobile.js
@@ -1,56 +1,59 @@
 import React from 'react'
 import './boardingPassMobile.css'
-import { bookingInfo } from '../../orderData'
 
-// idk why the resolution is like crap
 import boardingPassMobile from '../../images/boardingPassMobile.png'
 import boardingPassStampMobile from '../../images/boardingPassStampMobile.png'
 
-const {
-  bookingId,
-  bookingDate,
-  orderPlaceDate,
-  startTime,
-  bookingName,
-  bookingContact,
-  people,
-  storeName,
-} = bookingInfo
+const BoardingPassMobile = (props) => {
+  const today = new Date().toISOString().split('T')[0]
 
-const BoardingPassMobile = () => {
+  const {
+    date,
+    numberOfPeople,
+    reservationId,
+    startTime,
+    storeName,
+    userName,
+    userPhone,
+  } = props
+
   return (
     <div className="boarding-pass-mobile-wrapper" style={{ fontSize: '12px' }}>
       <img
         className="boarding-pass"
-        src={bookingId ? boardingPassStampMobile : boardingPassMobile}
+        src={reservationId ? boardingPassStampMobile : boardingPassMobile}
         alt="boarding pass"
       />
       <div className="booking-info-wrapper-m">
         <div className="booking-main-info-row-m">
           <div className="booking-info-title-m">訂單編號</div>
           <div className="booking-info-content-m">
-            {bookingId || <i>待確認</i>}
+            {reservationId || <i>待確認</i>}
           </div>
         </div>
         <div className="booking-main-info-row-m">
           <div className="booking-info-title-m">訂位大名</div>
-          <div className="booking-info-content-m">{bookingName}</div>
+          <div className="booking-info-content-m">{userName}</div>
         </div>
         <div className="booking-main-info-row-m">
           <div className="booking-info-title-m">訂單成立日</div>
-          <div className="booking-info-content-m">{orderPlaceDate}</div>
+          <div className="booking-info-content-m">{today}</div>
         </div>
         <div className="booking-main-info-row-m">
           <div className="booking-info-title-m">聯絡資訊</div>
-          <div className="booking-info-content-m">{bookingContact}</div>
+          <div className="booking-info-content-m">
+            {userPhone || '0926554778'}
+          </div>
         </div>
         <div className="booking-main-info-row-m">
           <div className="booking-info-title-m">人數</div>
-          <div className="booking-info-content-m">{people} 人</div>
+          <div className="booking-info-content-m">
+            {numberOfPeople + ' - ' + (Number(numberOfPeople) + 2)} 人
+          </div>
         </div>
         <div className="booking-main-info-row-m">
           <div className="booking-info-title-m">訂位日期</div>
-          <div className="booking-info-content-m">{bookingDate}</div>
+          <div className="booking-info-content-m">{date}</div>
         </div>
         <div className="booking-main-info-row-m">
           <div className="booking-info-title-m">訂位時間</div>
