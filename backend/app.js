531cde6128c8dfc20ec45aa1d2ac7ab4270ecc58
hannahfang0807 <82069223+hannahfang0807@users.noreply.github.com>
2021-07-20 00:21:39 +0800

    member與login相關的app.js調整

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
+// session相關
+var session = require('express-session');
+// const MysqlStore = require('express-mysql-session')(session);
+// const db = require(__dirname + '/database/db.connect');
+// const sessionStore = new MysqlStore({}, db);
 
 var indexRouter = require('./routes/index');
 var bookingRouter = require('./routes/booking');
@@ -11,8 +16,30 @@ var userRouter = require('./routes/user');
 var cartRouter = require('./routes/cart');
 var messageRouter = require('./routes/message');
 var itemRouter = require('./routes/items');
+var memberRouter = require('./routes/members');
+// var cartRouter = require('./routes/cart');
+// var messageRouter = require('./routes/message');
+// var itemRouter = require('./routes/items');
 
 var app = express();
+
+// session 
+app.use(
+  session({
+    cookieName: 'session',
+    secret: 'eg[isfd-8yF9-7w2315df{}+Ijsli;;to8',
+    duration: 30 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000,
+    // httpOnly: false, //for localhost
+    httpOnly: true, 
+    // secure: false, //for localhost
+    secure: true,
+    ephemeral: true,
+    resave: true,
+    saveUninitialized: true,
+  })
+)
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,14 +52,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter); 
 app.use('/booking', bookingRouter);
 
 // 先comment掉 因為檔案目前沒有export東西會噴錯誤
 // app.use('/users', userRouter);
+app.use('/members', memberRouter);
 // app.use('/cart', cartRouter);
 // app.use('/message', messageRouter);
 // app.use('/items', itemRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,11 +12,20 @@ import ReservationIndex from './pages/reservations/ReservationIndex'
 // import ItemIndex from './pages/items/ItemIndex'
 // import CartIndex from './pages/cart/CartIndex'
 // import MessageIndex from './pages/message/MessageIndex'
+//Hannah
+import MemberIndex from './pages/member/MemberIndex'
+import MemberEdit from './pages/member/MemberEdit'
+import MemberFavList from './pages/member/MemberFavList'
+import MemberOrder from './pages/member/MemberOrder'
+import MemberCoupon from './pages/member/MemberCoupon'
+import MemberPwdEdit from './pages/member/MemberPwdEdit'
+import MemberPwdEditSuccess from './pages/member/MemberPwdEditSuccess'
+import LoginIndex from './pages/login/LoginIndex'
+import Register from './pages/login/Register'
+import ForgetPwd from './pages/login/ForgetPwd'
+import ForgetPwdMailSent from './pages/login/ForgetPwdMailSent'
 
 const App = () => {
-  // 會員登入： 1. 在這邊設立狀態然後分別傳下去嗎？ 2. 有沒有其他做法
-  const [auth, setAuth] = useState(false)
-
   return (
     <Router>
       <Navbar />
@@ -24,6 +33,40 @@ const App = () => {
         {/* <Homepage />
         <About />
         <MemberIndex /> */}
+        {/* Hannah */}
+        <Route path="/login">
+          <LoginIndex />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/forgetPwd">
+          <ForgetPwd />
+        </Route>
+        <Route path="/forgetPwdMailSent">
+          <ForgetPwdMailSent />
+        </Route>
+        <Route path="/member/memberPwdEditSuccess">
+          <MemberPwdEditSuccess />
+        </Route>
+        <Route path="/member/memberPwdEdit">
+          <MemberPwdEdit />
+        </Route>
+        <Route path="/member/memberCoupon">
+          <MemberCoupon />
+        </Route>
+        <Route path="/member/memberOrder">
+          <MemberOrder />
+        </Route>
+        <Route path="/member/memberEdit">
+          <MemberEdit />
+        </Route>
+        <Route path="/member/memberFavList">
+          <MemberFavList />
+        </Route>
+        <Route path="/member">
+          <MemberIndex />
+        </Route>
         <ReservationIndex />
         {/* <ItemIndex />
         <CartIndex />
