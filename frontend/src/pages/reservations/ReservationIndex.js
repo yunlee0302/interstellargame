22bb38613c4584377b19ff2067b07f9de30a5ebd
JCEleanor <crcky4826@gmail.com>
2021-07-21 22:57:23 +0800

    預約：連至會員中心＆商城，remove booking-related localStorage

--- frontend/src/pages/reservations/ReservationIndex.js
+++ frontend/src/pages/reservations/ReservationIndex.js
@@ -5,18 +5,19 @@ import BookingStep from './pages/BookingStep'
 import BookingProcess from './pages/BookingProcess'
 import BookingConfirm from './pages/BookingConfirm'
 import BookingSuccess from './pages/BookingSuccess'
+import MemberBooking from './pages/MemberBooking'
 
 const reservationIndex = () => {
-    return (
-        <>
-            <Route path="/booking/steps" exact component={BookingStep} />
-            {/* todo: 我的下面三頁也要用到會員登入狀態 待改 */}
-            <Route path="/booking" exact component={BookingProcess} />
-            <Route path="/booking/confirm" exact component={BookingConfirm} />
-            <Route path="/booking/success/:id" component={BookingSuccess} />
-        </>
-
-    )
+  return (
+    <>
+      <Route path="/booking/steps" exact component={BookingStep} />
+      {/* todo: 我的下面三頁也要用到會員登入狀態 待改 */}
+      <Route path="/booking" exact component={BookingProcess} />
+      <Route path="/booking/confirm" exact component={BookingConfirm} />
+      <Route path="/booking/success/:id" component={BookingSuccess} />
+      <Route path="/member/booking/:id" component={MemberBooking} />
+    </>
+  )
 }
 
 export default reservationIndex
